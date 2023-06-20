f09d869ec5f4411c750c4e5a975c194533944ab7
Trashslamdunk <137202647+Trashslamdunk@users.noreply.github.com>
2023-06-20 19:36:26 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+"use strict"
+
+import musicPlayer from './modules/musicPlayer.js';
+// import burgerMenu from './modules/burger.js';
+// import headerSticky from './modules/header-sticky.js';
+musicPlayer()
+// headerSticky();
+// burgerMenu();
