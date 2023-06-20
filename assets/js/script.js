1a8824d561290d2418a67dc819e2bb27e52dc089
Trashslamdunk <137202647+Trashslamdunk@users.noreply.github.com>
2023-06-20 21:12:07 +0300

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-import musicPlayer from './modules/musicPlayer.js';
-// import burgerMenu from './modules/burger.js';
-// import headerSticky from './modules/header-sticky.js';
-musicPlayer()
-// headerSticky();
-// burgerMenu();
